4ede69932078454aa6c733d46ddb60cd23299555
creightonja <creightonja@gmail.com>
2016-04-11 12:48:42 +0900

    Vue.js task app

---
+++ task.js
@@ -0,0 +1,12 @@
+new Vue({
+
+    el: '#app',
+
+    data: {
+        tasks: [
+            {body: 'Goto the store', completed: false },
+            {body: 'Wash dog', completed: false },
+            {body: 'Clean car', completed: true }
+        ]
+    }
+})
